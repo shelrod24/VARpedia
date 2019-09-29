72132fd5312fb4b66b4ec905e39e5f92d4af9edf
UltimaKuma <ChinL@outlook.co.nz>
2019-09-29 16:35:27 +1300

    fully implemented dark theme

---
+++ src/module-info.java
@@ -0,0 +1,13 @@
+module WikiSpeak {
+	exports Services;
+	opens controllers;
+	requires javafx.base;
+	requires javafx.controls;
+	requires javafx.fxml;
+	requires javafx.graphics;
+	requires javafx.media;
+	requires flickr4java;
+	requires java.desktop;
+	
+	exports controllers;
+}
