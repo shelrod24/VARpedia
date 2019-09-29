ded488e2e100fc96a490328030ebeaf3de22da42
Sheldon Rodricks <srdo536@en-369726.uoa.auckland.ac.nz>
2019-09-29 21:50:00 +1300

    added runnable jar

---
+++ Submission/scripts/delete_finals.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# No Inputs
+
+FINAL_DIR="./temps/final"
+mkdir -p $FINAL_DIR
+
+#delete all within directory
+rm "${FINAL_DIR}/"*
---
+++ Submission/scripts/make_directories.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+#No Input
+mkdir -p ./creations
+mkdir -p ./audio
+mkdir -p ./temps/final
+mkdir -p ./temps/image
