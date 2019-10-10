7da8e447398a90434c754a94d9922d2d00666f77
Sheldon Rodricks <srdo536@en-369813.uoa.auckland.ac.nz>
2019-10-11 12:29:33 +1300

    fixed small fault due to outdated ffmpeg in lab comps

--- scripts/temps/festival/chunk.scm
+++
@@ -1,4 +0,0 @@
-(voice_kal_diphone)
-(utt.save.wave (SayText "In June 2018, the 1964 250 GTO became the most expensive car in history, setting an all-time record selling price of $70 million.
-FCA announced its intentions to separate Ferrari S.p.A.
-") "adhg.wav" `riff)
