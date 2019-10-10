83d13bd2ea0868ac2c77f8478101a23cb768c6aa
Sheldon Rodricks <srdo536@en-369813.uoa.auckland.ac.nz>
2019-10-11 12:27:19 +1300

    fixed small fault due to outdated ffmpeg in lab comps

--- scripts/mix_audio.sh
+++ scripts/mix_audio.sh
@@ -12,5 +12,5 @@ mkdir -p ${MUSIC_DIR}
 if [ -z $1 ] ; then
 	cp -f "${FINAL_DIR}/TEMP.mp3" "${FINAL_DIR}/TEMPFINAL.mp3"
 else
-	ffmpeg -y -i "${FINAL_DIR}/TEMP.mp3" -stream_loop -1 -i "${MUSIC_DIR}/${1}"  -filter_complex amix=inputs=2:duration=first:dropout_transition=1 "${FINAL_DIR}/TEMPFINAL.mp3"
+	ffmpeg -y -i "${FINAL_DIR}/TEMP.mp3" -i "${MUSIC_DIR}/${1}" -filter_complex amix=inputs=2:duration=first:dropout_transition=1 "${FINAL_DIR}/TEMPFINAL.mp3"
 fi
