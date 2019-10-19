7ed52ddf24a1381c1851898ecda9e3a06f7c23ef
UltimaKuma <ChinL@outlook.co.nz>
2019-10-20 12:50:01 +1300

    replaced aplay in DeleteAudio with MediaPlayer

--- src/controllers/DeleteAudio.java
+++ src/controllers/DeleteAudio.java
@@ -10,172 +10,185 @@ import javafx.scene.control.Button;
 import javafx.scene.control.ButtonType;
 import javafx.scene.control.DialogPane;
 import javafx.scene.control.ListView;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
+
+import java.io.File;
 import java.io.IOException;
 import java.util.List;
 
 public class DeleteAudio extends Controller {
 
 
-    private String _previousscene = "/fxml/MainMenu.fxml";
-    private String _subdirectory;
-    private String _audio;
-    @FXML private Button _previewbutton;
-    @FXML private ListView<String> _audiosubdirs;
-    @FXML private ListView<String> _audiofiles;
+	private String _previousscene = "/fxml/MainMenu.fxml";
+	private String _subdirectory;
+	private String _audio;
+	private MediaPlayer _player;
+
+	@FXML private Button _previewbutton;
+	@FXML private ListView<String> _audiosubdirs;
+	@FXML private ListView<String> _audiofiles;
 
 
 
-    public void SelectAudioSubDirectory() {
+	public void SelectAudioSubDirectory() {
 
-        _audiofiles.getItems().clear();
-        _subdirectory = _audiosubdirs.getSelectionModel().getSelectedItem();
+		_audiofiles.getItems().clear();
+		_subdirectory = _audiosubdirs.getSelectionModel().getSelectedItem();
 
-        if (_subdirectory == null) {
+		if (_subdirectory == null) {
 
-        } else {
+		} else {
 
-            List<String> audiofiles = DirectoryServices.ListFilesInDir("./audio/"+_subdirectory);
+			List<String> audiofiles = DirectoryServices.ListFilesInDir("./audio/"+_subdirectory);
 
-            for (String s : audiofiles) {
-            	if (s.endsWith(".wav")) {
-                    _audiofiles.getItems().add(s);
+			for (String s : audiofiles) {
+				if (s.endsWith(".wav")) {
+					_audiofiles.getItems().add(s);
 				}
-            }
+			}
 
-        }
+		}
 
-    }
-
-
-
-     public void DeleteAudiofile() throws IOException, InterruptedException {
-
-         _audio = _audiofiles.getSelectionModel().getSelectedItem();
-
-         if (_audio == null) {
-
-             return;
-
-         }
-
-         Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
-         alert.setTitle("File Deletion Confirmation");
-         alert.setHeaderText(null);
-         alert.setContentText("Are you sure you want to delete " + _audio + "?");
-         DialogPane pane = alert.getDialogPane();
-         pane.getStylesheets().add(getClass().getResource("/css/dark.css").toExternalForm());
-         pane.setId("background");
-         alert.getButtonTypes().setAll(ButtonType.YES, ButtonType.NO);
-         alert.showAndWait();
-
-         if (alert.getResult() == ButtonType.NO) {
-
-             return;
-
-         } else {
-
-        	 // deleting original audio
-             this.ProcessRunner("/bin/bash", "rm -f " + "\"./audio/" + _subdirectory + "/" + _audio + "\"");
-             // deleting redacted audio
-             String redactedAudio = _audio.substring(0, _audio.length()-4) + "_redacted.wav";
-             this.ProcessRunner("/bin/bash", "rm -f " + "\"./audio/" + _subdirectory + "/redacted/" + redactedAudio + "\"");
-
-
-             List<String> audiofiles = DirectoryServices.ListFilesInDir("./audio/"+_subdirectory);
-             _audiofiles.getItems().clear();
-
-             for (String s : audiofiles) {
-            	 if (s.endsWith(".wav")) {
-            		 _audiofiles.getItems().add(s);
-            	 }
-             }
-
-             //check if redacted is empty
-             this.ProcessRunner("sh", "./scripts/checkdir_isempty.sh" + " \"" + _subdirectory + "/redacted\"");
-             //check if subdirectory is empty
-             this.ProcessRunner("sh", "./scripts/checkdir_isempty.sh" + " \"" + _subdirectory + "\"");
-
-
-             _audiosubdirs.getItems().clear();
-             List<String> _audiosubfolders = DirectoryServices.ListFilesInDir("./audio");
-
-             for (String s : _audiosubfolders) {
-
-                 _audiosubdirs.getItems().add(s);
-
-             }
-
-         }
-
-     }
+	}
 
 
 
-     @FXML
-     public void initialize() throws InterruptedException, IOException {
+	public void DeleteAudiofile() throws IOException, InterruptedException {
+
+		_audio = _audiofiles.getSelectionModel().getSelectedItem();
+
+		if (_audio == null) {
+
+			return;
+
+		}
+
+		Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+		alert.setTitle("File Deletion Confirmation");
+		alert.setHeaderText(null);
+		alert.setContentText("Are you sure you want to delete " + _audio + "?");
+		DialogPane pane = alert.getDialogPane();
+		pane.getStylesheets().add(getClass().getResource("/css/dark.css").toExternalForm());
+		pane.setId("background");
+		alert.getButtonTypes().setAll(ButtonType.YES, ButtonType.NO);
+		alert.showAndWait();
+
+		if (alert.getResult() == ButtonType.NO) {
+
+			return;
+
+		} else {
+
+			// deleting original audio
+			this.ProcessRunner("/bin/bash", "rm -f " + "\"./audio/" + _subdirectory + "/" + _audio + "\"");
+			// deleting redacted audio
+			String redactedAudio = _audio.substring(0, _audio.length()-4) + "_redacted.wav";
+			this.ProcessRunner("/bin/bash", "rm -f " + "\"./audio/" + _subdirectory + "/redacted/" + redactedAudio + "\"");
 
 
-         // Remove all empty directories.
-         ProcessBuilder deletedirs = new ProcessBuilder("sh", "-c", "./scripts/delete_all_nonemptydirs.sh");
-         Process deletedirprocess = deletedirs.start();
-         deletedirprocess.waitFor();
+			List<String> audiofiles = DirectoryServices.ListFilesInDir("./audio/"+_subdirectory);
+			_audiofiles.getItems().clear();
 
-        // Populate ListView
-         List<String> _audiosubfolders = DirectoryServices.ListFilesInDir("./audio");
+			for (String s : audiofiles) {
+				if (s.endsWith(".wav")) {
+					_audiofiles.getItems().add(s);
+				}
+			}
 
-         for(String s: _audiosubfolders) {
-
-             _audiosubdirs.getItems().add(s);
-
-         }
-
-     }
-
-     public void PreviewExistingAudio () throws IOException, InterruptedException {
-    	 
-    	 if(_audiofiles.getSelectionModel().getSelectedItem()==null) {
-    		 return;
-    	 }
-
-        _previewbutton.setDisable(true);
-
-        Thread worker = new Thread(new Task<Void>() {
-
-            @Override
-            protected Void call() throws Exception {
-
-                _audio = _audiofiles.getSelectionModel().getSelectedItem();
-                AudioService.playAudio(_subdirectory, _audio);
-                return null;
-
-            }
-
-            @Override
-            protected void done() {
-
-                Platform.runLater(()->{
-
-                    _previewbutton.setDisable(false);
-
-                });
-
-            }
-
-        });
-
-        worker.start();
-
-     }
+			//check if redacted is empty
+			this.ProcessRunner("sh", "./scripts/checkdir_isempty.sh" + " \"" + _subdirectory + "/redacted\"");
+			//check if subdirectory is empty
+			this.ProcessRunner("sh", "./scripts/checkdir_isempty.sh" + " \"" + _subdirectory + "\"");
 
 
-    @Override
-    public String ReturnFXMLPath() {
-        return _previousscene;
-    }
+			_audiosubdirs.getItems().clear();
+			List<String> _audiosubfolders = DirectoryServices.ListFilesInDir("./audio");
 
-    @Override
-    public String ReturnForwardFXMLPath() {
-        return null;
-    }
+			for (String s : _audiosubfolders) {
+
+				_audiosubdirs.getItems().add(s);
+
+			}
+
+		}
+
+	}
+
+
+
+	@FXML
+	public void initialize() throws InterruptedException, IOException {
+
+		// Remove all empty directories.
+		ProcessBuilder deletedirs = new ProcessBuilder("sh", "-c", "./scripts/delete_all_nonemptydirs.sh");
+		Process deletedirprocess = deletedirs.start();
+		deletedirprocess.waitFor();
+
+		// Populate ListView
+		List<String> _audiosubfolders = DirectoryServices.ListFilesInDir("./audio");
+
+		for(String s: _audiosubfolders) {
+
+			_audiosubdirs.getItems().add(s);
+
+		}
+
+	}
+
+	public void PreviewExistingAudio() throws IOException, InterruptedException {
+
+		if(_audiofiles.getSelectionModel().getSelectedItem()==null) {
+			return;
+		}
+
+		if(_previewbutton.getText().equals("Play")) {
+			
+			// Change button to stop
+			_previewbutton.setText("Stop");
+			Image image = new Image(getClass().getResourceAsStream("/icons/stop.png"));
+			_previewbutton.setGraphic(new ImageView(image));
+			
+			// Play audio
+			_audio = _audiofiles.getSelectionModel().getSelectedItem();
+			File fileUrl = new File("./audio/" + _subdirectory + "/" + _audio);
+			Media audio = new Media(fileUrl.toURI().toString());
+			_player = new MediaPlayer(audio);
+			_player.autoPlayProperty().set(true);
+			
+			// At the end, call method again to reset button
+			_player.setOnEndOfMedia(new Runnable() {
+				@Override
+				public void run() {
+					try {
+						PreviewExistingAudio();
+					} catch (IOException | InterruptedException e) {
+						e.printStackTrace();
+					}
+				}
+			});
+			
+		} else if(_previewbutton.getText().equals("Stop") && _player!=null) {
+			// Stop player and set button to play
+			_player.stop();
+			_previewbutton.setText("Play");
+			Image image = new Image(getClass().getResourceAsStream("/icons/play.png"));
+			_previewbutton.setGraphic(new ImageView(image));
+		}
+
+	}
+
+
+	@Override
+	public String ReturnFXMLPath() {
+		return _previousscene;
+	}
+
+	@Override
+	public String ReturnForwardFXMLPath() {
+		return null;
+	}
 
 }
