358b9d9cf3cca21acd85e3da243faae64d346e58
Leon Chin <ChinL@outlook.co.nz>
2019-10-25 12:40:57 +1300

    commented FlickrAPIService

--- src/services/FlickrAPIService.java
+++ src/services/FlickrAPIService.java
@@ -14,20 +14,15 @@ import com.flickr4java.flickr.photos.*;
 
 public class FlickrAPIService {
 	public static String getAPIKey(String key) throws Exception {
-		// TODO fix the following based on where you will have your config file stored
-
-//		String config = System.getProperty("user.dir") 
-//				+ System.getProperty("file.separator")+ "flickr-api-keys.txt"; 
-		
-//		String config = System.getProperty("user.home")
-//				+ System.getProperty("file.separator")+ "bin" 
-//				+ System.getProperty("file.separator")+ "flickr-api-keys.txt"; 
+		//open file flickr_api_keys.txt
 		File file = new File("flickr_api_keys.txt"); 
 		BufferedReader br = new BufferedReader(new FileReader(file)); 
 		
+		//split line by =, and check if it is the right required key as specified by input
 		String line;
 		while ( (line = br.readLine()) != null ) {
 			if (line.trim().startsWith(key)) {
+				//if right key, close reader and return key
 				br.close();
 				return line.substring(line.indexOf("=")+1).trim();
 			}
@@ -39,6 +34,7 @@ public class FlickrAPIService {
 	public static List<String> getImages(String keyword, int resultsPerPage, int page) {
 		List<String> imageFilenames = new ArrayList<String>();
 		try {
+			// get the two API keys
 			String apiKey = getAPIKey("apiKey");
 			String sharedSecret = getAPIKey("sharedSecret");
 
@@ -46,6 +42,7 @@ public class FlickrAPIService {
 			
 			String query = keyword;
 			
+			// set up the quesry
 	        PhotosInterface photos = flickr.getPhotosInterface();
 	        SearchParameters params = new SearchParameters();
 	        params.setSort(SearchParameters.RELEVANCE);
@@ -57,6 +54,7 @@ public class FlickrAPIService {
 	        	        
 	        for (Photo photo: results) {
 	        	try {
+	        		//get photos
 	        		BufferedImage image = photos.getImage(photo,Size.MEDIUM);
 		        	String filename = query.trim().replace(' ', '-')+"-"+System.currentTimeMillis()+"-"+photo.getId()+".jpg";
 		        	File outputfile = new File("temps/image",filename);
@@ -68,6 +66,7 @@ public class FlickrAPIService {
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
+		// return image filenames
 		return imageFilenames;
 	}	
 }
