8d8b7e558f412d6c2fdc1d93c70fafb7b9d28eef
Sheldon Rodricks <srdo536@en-368397.uoa.auckland.ac.nz>
2019-10-27 16:05:41 +1300

    removed out folder

--- out/artifacts/JavaFxApplication_jar/scripts/list_voices.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# No inputs
-
-echo `echo "(voice.list)" | festival -i | grep "festival> (" | cut -d "(" -f2 | cut -d ")" -f1`
-
--- out/artifacts/JavaFxApplication_jar/scripts/make_directories.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-#No Input
-mkdir -p ./creations
-mkdir -p ./audio
-mkdir -p ./temps/final
-mkdir -p ./temps/image
